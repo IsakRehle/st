7db9fb7395e6afe620c431251bed01d69bee3ade
Isak Rehle <isak@rehle.xyz>
2022-12-01 12:00:00 +0100

    modified:   config.h

--- config.h
+++ config.h
@@ -6,8 +6,10 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 /* static char *font = "mono:pixelsize=16:antialias=true:autohint=true"; */
-static char *font = "DejaVu Sans Mono:weight=medium:pixelsize=16:antialias=true:autohint=true";
+
 /* static char *font = "Inconsolata:style=Regular:pixelsize=16:antialias=true:autohint=true"; */
+/* static char *font = "DejaVu Sans Mono:weight=medium:pixelsize=15:antialias=true:autohint=true"; */
+static char *font = "DejaVu Sans Mono:style=Book:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
